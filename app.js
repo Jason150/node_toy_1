90823e4cf77be2d5f5d9b51a513d3e8ceb39be35
Jason150 <31621587+Jason150@users.noreply.github.com>
2018-08-27 20:43:18 +0900

    add app.js post, put delete

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.get('/', function (req, res) {
   res.send('안녕 이다 이 새끼야');
 });
 
+app.post('/', function (req, res) {
+  res.send('이건 포스트다 이 새끼야');
+});
+
+app.put('/user', function (req, res) {
+  res.send('이건 풋이다 이 새끼야');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('이건 딜리트다 이 새끼야');
+});
+
 app.listen(3000, function () {
   console.log('Examle app listening on port 3000!')
 });
